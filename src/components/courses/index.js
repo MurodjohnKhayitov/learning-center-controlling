2245570ff0fc28b963c450b985698c869eb080e3
IsmoilKarimov <karimoovismooil@gmail.com>
2022-12-12 12:03:46 +0500

    added some infos

---
+++ src/components/courses/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import './courses.scss'
+
+const Courses = ({courses}) => {
+    return(
+        <div className="courses">
+            <div className="container">
+                <div className="courses__list">
+                    <div className="courses__item">
+                        <div className="courses__author">
+                            <div className='courses__author--img'></div>
+                            <NavLink className="courses__author--name">Aliyev Vali</NavLink>
+                            <NavLink className="courses__author--spec">Frontend</NavLink>
+                        </div>
+                        <div className="courses__info">
+                            <div className="courses__top">
+                                <NavLink className='courses__top--pro'>Jadval</NavLink>
+                                <NavLink className='courses__top--time'>Davomiyligi: 22 soat</NavLink>
+                                <NavLink className='courses__top--duration'>Darslar soni: 100 ta</NavLink>
+                            </div>
+                            <h2>Front-end mutaxassislik kursi</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam veritatis sint, error, odit quas dolor placeat quod nihil similique inventore recusandae odio praesentium cum doloremque in blanditiis doloribus voluptates excepturi.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Courses
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,37 @@
 import React from "react";
+import Courses from "../components/courses";
 
 const Home = () => {
+    const courses = [
+        {
+            id:0,
+            img: 'https://postila.ru/data/a5/ef/14/bd/a5ef14bdafa593594d27d9b6473f35a4132ca79d0f4f5d4eac0b7edbe14c62a2.jpg',
+            duration: '100 ta',
+            time: '22 soat',
+            course: 'Frontend mutaxasislik kursi',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            author: {
+                name: 'Aliyev Vali',
+                speciality: 'Frontend'
+            }
+        },
+        {
+            id:1,
+            img: 'https://yandex.ru/images/search?text=images+person&img_url=http%3A%2F%2Fi.pinimg.com%2Foriginals%2Ffc%2F67%2F03%2Ffc6703e79d41363832817cbdf297beaa.jpg&pos=7&rpt=simage&stype=image&lr=10335&parent-reqid=1670821692631829-17426207716315213589-sas3-0973-1aa-sas-l7-balancer-8080-BAL-5901&source=serp',
+            duration: '93 ta',
+            time: '25 soat',
+            course: 'Backend mutaxasislik kursi',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            author: {
+                name: 'Berdiyev Javohir',
+                speciality: 'Backend'
+            }
+        },
+    ]
     return(
         <>
-            <h1>Barcha kurslar ro'yhati</h1>
+            <h1 className="btitle">Barcha kurslar ro'yhati</h1>
+            <Courses courses={courses}/>
 
         </>
     )
